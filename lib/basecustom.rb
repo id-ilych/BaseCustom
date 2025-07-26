9abe5764960a8af4bc25c4d57058fecee55500f3
Ilya Denisov <id.ilych@gmail.com>
2025-07-26 20:40:50 +0300

    Raise on negative integer input

--- lib/basecustom.rb
+++ lib/basecustom.rb
@@ -45,6 +45,7 @@ class BaseCustom
       i_out
     # end input_val.is_a?(String)
     elsif input_val.is_a?(Integer)
+       raise "Negative integers are not supported!" if input_val < 0
        return @BASE_PRIMITIVES_HASH.first[0] if input_val == 0
        number = input_val
        result = ""
